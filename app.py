820baaabb7689f2b4ed29f9d1b0f8c9528486382
Jeremy Arimado <jarimado@gmail.com>
2023-06-30 05:15:20 +1000

    add search and chat

--- app.py
+++ app.py
@@ -2,16 +2,19 @@ import os
 from flask import Flask, request
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain
+from langchain.chains import LLMChain, ConversationChain
 from langchain.memory import ConversationBufferMemory
-from langchain.utilities import WikipediaAPIWrapper
+from langchain.agents import AgentType, initialize_agent, load_tools
 
 
 app = Flask(__name__)
 
 apiKey = os.getenv('OPEN_AI_API_KEY')
 os.environ['OPENAI_API_KEY'] = apiKey
+serpapi_api_key = os.getenv('SERPAPI_API_KEY')
+os.environ['SERPAPI_API_KEY'] = serpapi_api_key
 llm = OpenAI(temperature=1.4)
+llmSearch = OpenAI(temperature=0)
 
 title_template = PromptTemplate(
     input_variables=['topic'],
@@ -23,6 +26,16 @@ title_memory = ConversationBufferMemory(
 
 title_chain = LLMChain(llm=llm, prompt=title_template,
                        verbose=True, output_key='title', memory=title_memory)
+
+tools = load_tools(["serpapi", "llm-math"], llm=llm)
+
+searchAgent = initialize_agent(
+    tools, llmSearch, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+# mathAgent = initialize_agent(tools, llm, agent=AgentType.MATH, verbose=true);
+# chatAgent = initialize_agent(tools, llm, agent=AgentType.CHAT, verbose=true);
+# reactAgent = initialize_agent(tools, llm, agent=AgentType.REACT_DESCRIPTION, verbose=true);
+
+conversation = ConversationChain(llm=llm, verbose=True)
 
 stores = [
     {
@@ -39,6 +52,22 @@ stores = [
 
 @app.post("/chat")
 def chat():
+    request_data = request.get_json()
+    message = request_data["message"]
+    response = conversation.run(message)
+    return {"response": response}
+
+
+@app.post("/search")
+def search():
+    # get search pramater from request
+    search = request.args.get('query')
+    result = searchAgent.run(search)
+    return {"result": result}
+
+
+@app.post("/title")
+def title():
     request_data = request.get_json()
     topic = request_data["topic"]
     title = title_chain.run(topic)
